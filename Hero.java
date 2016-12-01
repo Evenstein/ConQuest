6c1f49ded75e30820f813c14a474ead3a84537e8
Vitaly Sokolsky <star78@tut.by>
2016-12-01 12:08:34 +0300

    01/12 Hero Description

---
+++ Hero.java
@@ -0,0 +1,53 @@
+package com.evenstein.conquest;
+
+/**
+ * Created by evenstein on 01.12.16.
+ */
+public class Hero {
+
+    private int heroHitPoints;
+    private int heroStrength;
+    private int heroDexterity;
+    private int heroMagicka;
+    private int heroMana;
+
+    public int getHeroStrength() {
+        return heroStrength;
+    }
+
+    public void setHeroStrength(int heroStrength) {
+        this.heroStrength = heroStrength;
+    }
+
+    public int getHeroDexterity() {
+        return heroDexterity;
+    }
+
+    public void setHeroDexterity(int heroDexterity) {
+        this.heroDexterity = heroDexterity;
+    }
+
+    public int getHeroMagicka() {
+        return heroMagicka;
+    }
+
+    public void setHeroMagicka(int heroMagicka) {
+        this.heroMagicka = heroMagicka;
+    }
+
+    public int getHeroHitPoints() {
+        return heroHitPoints;
+    }
+
+    public void setHeroHitPoints(int heroHitPoints) {
+        this.heroHitPoints = heroHitPoints;
+    }
+
+    public int getHeroMana() {
+        return heroMana;
+    }
+
+    public void setHeroMana(int heroMana) {
+        this.heroMana = heroMana;
+    }
+}
